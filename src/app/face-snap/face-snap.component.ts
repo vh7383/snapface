67837852cf18d264117303ae92499aa651e61369
vh7383 <vincent.hernandes@gmail.com>
2023-04-14 21:17:31 +0200

    face-snap component with style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  // le bang (!) "promet" a typescript que ces variables seront initialisées
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
 
+  ngOnInit() {
+    // on initialise les variables
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis tout petit';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+  }
 }
