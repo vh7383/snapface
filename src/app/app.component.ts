0e7af78e506651cd38954d76022681f4009e43c4
vh7383 <vincent.hernandes@gmail.com>
2023-04-15 08:26:32 +0200

    model FaceSnap & injectable property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FaceSnap} from "./models/face-snap.model";
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // propriété injectable dans le template
+  mySnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = new FaceSnap(
+      'Archibald',
+      'Mon meilleur ami depuis tout petit',
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      new Date(),
+      0
+    );
+  }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,35 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FaceSnap} from "../models/face-snap.model";
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent implements OnInit {
-  // le bang (!) "promet" a typescript que ces variables seront initialisées
-  title!: string;
-  description!: string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl!: string;
+export class FaceSnapComponent{
+  // modèle injectable depuis le composant parent
+  @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
-  ngOnInit() {
-    // on initialise les variables
-    this.title = 'Archibald';
-    this.description = 'Mon meilleur ami depuis tout petit';
-    this.createdDate = new Date();
-    this.snaps = 6;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
-    this.buttonText = 'Oh Snap!';
-  }
-  // la méthode commence par 'on', signifie la réaction à un évenement
   onSnap() {
     if (this.buttonText === 'Oh Snap!') {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.buttonText = 'Oops, unSnap!';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.buttonText = 'Oh Snap!';
     }
   }
